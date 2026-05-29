31dc14f4e6b19146b36371c8a70b009e574c7e9a
AVAGOO <avawakening@sohu.com>
2026-05-29 22:48:36 +0800

    功能/修改： 	.gitignore增加Dev_Info/  用于后面存储开发信息 	Tools/setup/Dockerfile	用于构建可以支持构建&运行px4的容器（下方） 	./Tools/docker_run_px4-dev  用于利用上面的镜像构建&运行容器 	multi-sim-cessna.bash， multi-sim-x500.bash，single-sim-cessna.bash,  single-sim-x500.bash	用于在容器中多机，单机仿真
    要提交的变更：
    	修改：     .gitignore
    	删除：     Tools/docker_run.sh
    	新文件：   Tools/docker_run_px4-dev.sh
    	modified:   Tools/setup/Dockerfile
    	new file:   multi-sim-cessna.bash
    	new file:   multi-sim-x500.bash
    	new file:   single-sim-x500.bash

---
+++ Tools/docker_run_px4-dev.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+PX4_DOCKER_REPO="px4-qgc:px4-v1.16.2"
+
+# docker hygiene
+
+#Delete all stopped containers (including data-only containers)
+#docker rm $(docker ps -a -q)
+
+#Delete all 'untagged/dangling' (<none>) images
+#docker rmi $(docker images -q -f dangling=true)
+
+echo "PX4_DOCKER_REPO: $PX4_DOCKER_REPO";
+
+PWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+HOST_SRC_DIR=$PWD/../
+CONTAINER_SRC_DIR=/home/ubuntu/PX4-Autopilot
+
+CCACHE_DIR=${HOME}/.ccache
+mkdir -p "${CCACHE_DIR}"
+
+docker run -it --rm -w "${CONTAINER_SRC_DIR}" \
+	--user="$(id -u):$(id -g)" \
+	--env=BRANCH_NAME \
+	--env=CCACHE_DIR="${CCACHE_DIR}" \
+	--volume=${CCACHE_DIR}:${CCACHE_DIR}:rw \
+	--volume=${HOST_SRC_DIR}:${CONTAINER_SRC_DIR}:rw \
+	${PX4_DOCKER_REPO} /bin/bash -c "$@"
---
+++ single-sim-cessna.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -z "$PX4_SRC_DIR" ]; then
+    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    PX4_SRC_DIR="$(cd "${SCRIPT_DIR}/" && pwd)"
+fi
+
+PX4_BUILD_DIR="${PX4_SRC_DIR}/build/px4_sitl_default"
+
+cd "${PX4_BUILD_DIR}/src/modules/simulation/gz_bridge"
+
+export PX4_GZ_MODEL_POSE="0,0"
+export PX4_SYS_AUTOSTART=4003
+export PX4_SIM_MODEL=gz_rc_cessna
+export GZ_IP=127.0.0.1
+
+echo "========== 启动 PX4 SITL 单机仿真 =========="
+echo "源码目录: ${PX4_SRC_DIR}"
+echo "构建目录: ${PX4_BUILD_DIR}"
+echo "机体: Cessna"
+echo "仿真器: Gazebo"
+echo "=========================================="
+
+"${PX4_BUILD_DIR}/bin/px4" -i 1
+
